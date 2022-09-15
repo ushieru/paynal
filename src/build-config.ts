18c60edab2155137432a5d4f920b4df1878089c8
ushieru <uzielcocolan@gmail.com>
2022-09-15 16:58:56 -0500

    Add build config

---
+++ src/build-config.ts
@@ -0,0 +1,17 @@
+import { version } from '../package.json'
+import { Config, SecureConfig } from './types'
+
+export const buildConfig = (config: Config): SecureConfig => {
+    if (config.server === undefined) throw 'Server is required'
+    const secureConfig: SecureConfig = {
+        server: config.server,
+        serverName: config.serverName ?? 'Colibri/' + version,
+        path: config.path ?? '/ws',
+        heartBeat: config.heartBeat ?? [0, 0],
+        heartbeatErrorMargin: config.heartbeatErrorMargin ?? 1000,
+        debug: config.debug ?? (() => { }),
+        protocol: config.protocol ?? 'ws',
+        protocolConfig: config.protocolConfig ?? {}
+    }
+    return secureConfig
+}
