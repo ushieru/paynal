a79f02c68757f3f8b32707f08365e628a8835438
ushieru <uzielcocolan@gmail.com>
2022-09-15 17:01:42 -0500

    Add Frame test

---
+++ __test__/test.test.ts
@@ -0,0 +1,43 @@
+// @vitest-environment node
+import { expect, test, describe } from 'vitest'
+import { Frame } from './../src/frame'
+
+describe('Frame test', () => {
+    test('Build frame from string', () => {
+        const payload = 'CONNECT\nlogin:guest\npasscode:guest\n\nbody\x00'
+
+        const frame = Frame.fromPayload(payload)
+
+        expect(frame.command).toBe('CONNECT')
+        expect(frame.headers).toStrictEqual({ login: 'guest', passcode: 'guest' })
+        expect(frame.body).toBe('body')
+    })
+
+    test('Build frame from Buffer', () => {
+        const payload = Buffer.from('CONNECT\nlogin:guest\npasscode:guest\n\nbody\x00')
+
+        const frame = Frame.fromPayload(payload)
+
+        expect(frame.command).toBe('CONNECT')
+        expect(frame.headers).toStrictEqual({ login: 'guest', passcode: 'guest' })
+        expect(frame.body).toBe('body')
+    })
+
+    test('Build frame from constructor', () => {
+        const frame = new Frame('CONNECT', { login: 'guest', passcode: 'guest' }, 'body')
+
+        expect(frame.command).toBe('CONNECT')
+        expect(frame.headers).toStrictEqual({ login: 'guest', passcode: 'guest' })
+        expect(frame.body).toBe('body')
+    })
+
+    test('Build frame from string with json body', () => {
+        const payload = Buffer.from('SEND\ndestination:/queue/a\ncontent-type:application/json\n\n{"hello":"world"}\x00')
+
+        const frame = Frame.fromPayload(payload)
+
+        expect(frame.command).toBe('SEND')
+        expect(frame.headers).toStrictEqual({ destination: '/queue/a', 'content-type': 'application/json' })
+        expect(frame.body.hello).toBe('world')
+    })
+})
